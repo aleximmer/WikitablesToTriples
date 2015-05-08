3dcf34c0cce1f4c37adf371dbef5cd4022191630
Willi Raschkowski <willi.raschkowski@student.hpi.de>
2015-05-08 19:21:55 -0400

    ProblemTitles

--- getHTMLs.py
+++ getHTMLs.py
@@ -1,9 +1,12 @@
 import wikipedia
 
 listsFile = open('data/Titles.txt', 'r')
-problemsFile = open('data/ProblemTitles.txt', 'a')
+problemsFile = open('data/ProblemTitles.txt', 'w')
 
+count = 0
 for line in listsFile:
+    print count
+    count += 1
     try:
         page = wikipedia.page(line.decode('unicode-escape'))
     except Exception:
