8a7068c260916ff7d2e7dc15939d308549459abf
Willi Raschkowski <willi@raschkowski.com>
2015-05-25 22:15:44 +0200

    Added wikitables module

---
+++ wikitables/__init__.py
@@ -0,0 +1,2 @@
+from wikitables.page import Page
+from wikitables.table import Table
---
+++ wikitables/page.py
@@ -0,0 +1,46 @@
+import wikipedia
+from bs4 import BeautifulSoup
+import urllib
+from wikitables.table import Table
+
+class Page:
+
+    """This class abstracts Wikipedia articles to add table extraction functionality."""
+
+    _html = None
+    _soup = None
+    _tables = None
+
+    """Use 'contentOnly=True' if you want to filter 'See also' and 'References' sections."""
+    def __init__(self, title, contentOnly=True):
+        self.page = wikipedia.page(title)
+        self.title = self.page.title
+        self.url = self.page.url
+        self.contentOnly = contentOnly
+        if title != self.title:
+            print("Loaded \'%s\' instead of \'%s\'" % (self.title, title))
+
+    @property
+    def html(self):
+        if not self._html:
+            self._html = urllib.request.urlopen(self.url).read()
+        return self._html
+
+    @property
+    def soup(self):
+        if not self._soup:
+            self._soup = BeautifulSoup(self.html)
+        return self._soup
+
+    def categories(self):
+        return [a.text for a in self.soup.find(id='mw-normal-catlinks').findAll('a')]
+
+    @property
+    def tables(self):
+        if not self._tables:
+            self._tables = [Table(table) for table in self.soup.findAll('table', 'wikitable')]
+        return self._tables
+
+    @staticmethod
+    def filteredSections():
+        return ['References', 'See also']
