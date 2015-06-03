6e2fd7b7a9d58d94caf4c3f8ee714c2ba5989a9f
mockingbird2 <fabian.paul1@gmx.net>
2015-06-03 15:01:46 +0200

    setup python3 virtual env

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/fabian/Desktop/HPI/KnowMining/wiki-list_of-retrieval/venv/bin/python2
+#!/home/fabian/Desktop/HPI/KnowMining/wiki-list_of-retrieval/venv/bin/python3.4
 from django.core import management
 
 if __name__ == "__main__":
