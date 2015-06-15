12bf4da0a9e5e02dc7f221c764ad76c83233cf10
Willi Raschkowski <willi.raschkowski@student.hpi.de>
2015-06-15 12:34:39 +0200

    Improved printing

--- scripts/serverPredicates.py
+++ scripts/serverPredicates.py
@@ -13,17 +13,20 @@ with open('./TitlesShuffled.csv', 'r') as f:
     count = 0
 
     for title, *_ in titles:
+        count += 1
         path = savePath % title.replace('/', '\\')
-        print("%d\t%s" % (count, title))
         if os.path.isfile(path):
+            print("%d\texisting\t%s" % (count, title))
             continue
 
         try:
             page = w.Page(title)
             data = page.predicates(relative=True, omit=True)
         except Exception as e:
+            print("%d\terror\t\t%s" % (count, title))
             print(e)
             continue
         else:
             with open(path, 'w') as f:
                 json.dump(data, f, indent=4)
+                print("%d\t(dumped)\t%s" % (count, title))
