1827c3dae2b7c49d39547633632d32e5a21555fe
Fabian Paul <fabian.paul@student.hpi.de>
2015-07-09 01:12:41 +0200

    add possibility for accessing revisions of wikipedia

--- list_extraction/extensions/testing.py
+++ list_extraction/extensions/testing.py
@@ -3,5 +3,4 @@ from page import Page
 pg = Page('List of national parks of India')
 tb = pg.tables[0]
 print(pg.tables[0])
-print(tb.key)
-#print(tb.predicatesForColumns('Name', 'State'))
+# print(tb.key)
