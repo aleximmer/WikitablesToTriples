238067e947b4291fd95b6e281979d218f1c7be81
Fabian Paul <fabian.paul@student.hpi.de>
2015-07-09 11:03:01 +0200

    refactor revisions

--- list_extraction/extensions/page.py
+++ list_extraction/extensions/page.py
@@ -11,15 +11,12 @@ class Page:
     _soup = None
     _tables = None
 
-    def __init__(self, title, contentOnly=True, revisionID=None):
+    def __init__(self, title, revisionID='', contentOnly=True):
         """Use 'contentOnly=True' if you want to filter 'See also' and 'References' sections."""
         oldID = '&?&oldid='
-        if revisionID == None:
-            oldID = ''
-            revisionID = ''
         self.page = wikipedia.page(title)
         self.title = self.page.title
-        self.url = self.page.url + oldID + revisionID
+        self.url = self.page.url + oldID + str(revisionID)
         self.contentOnly = contentOnly
 
     def __repr__(self):
--- list_extraction/extensions/testing.py
+++ list_extraction/extensions/testing.py
@@ -1,6 +1,6 @@
 from page import Page
 
-pg = Page('List of national parks of India')
+pg = Page('List of national parks of India', 667908319)
 tb = pg.tables[0]
 print(pg.tables[0])
 # print(tb.key)
