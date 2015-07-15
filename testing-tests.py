ef7f5a771bb77dba8dcd42288c7f0d954167abac
Alexander Immer <alexander.immer@yahoo.com>
2015-07-15 17:04:38 +0200

    testing script

--- testing-tests.py
+++ testing-tests.py
@@ -1,6 +1,8 @@
 from wikitables.tests import *
 
 #test_column_names('List of national Parks of India', 'Name')
-test_key_extraction()
-test_key_predicates('List of national Parks of India')
+#test_key_extraction()
+#test_key_predicates('List of national Parks of India')
 #test_generate_RDFs('List of national Parks of India', 0.2)
+test_generate_RDFsForKey('List of national Parks of India')
+#test_match_key('List of national Parks of India')
